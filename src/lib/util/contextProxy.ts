eb003a407fe5923df1bc58b8f7d1719d7e528e8e
Daniel Nass <dnlnss@gmail.com>
2023-02-24 11:18:16 -0500

    Fix drawImage mask

--- src/lib/util/contextProxy.ts
+++ src/lib/util/contextProxy.ts
@@ -60,14 +60,16 @@ const createContextProxy = (context: CanvasRenderingContext2D) => {
           resizeCanvas();
         }
 
-        if (property === 'drawImage') {
-          proxyContext.fillRect(...(<Parameters<CanvasRect['fillRect']>>args));
-        }
-
         if (COLOR_OVERRIDES.includes(property)) {
           const layerColor = idToRgb(renderingLayerId());
           proxyContext.fillStyle = layerColor;
           proxyContext.strokeStyle = layerColor;
+        }
+
+        if (property === 'drawImage') {
+          proxyContext.fillRect(
+            ...(<Parameters<CanvasRect['fillRect']>>args.slice(1))
+          );
         }
 
         if (!EXCLUDED_GETTERS.includes(property)) {
