100956888f57b793c59bfa2340ef8d391e12ff2c
Daniel Nass <dnlnss@gmail.com>
2024-04-13 11:11:35 -0400

    Highlight getting started nav item

--- src/routes/_components/NavMenu.svelte
+++ src/routes/_components/NavMenu.svelte
@@ -7,7 +7,9 @@
 {#each Object.entries(items) as [title, value]}
   {#if typeof value === 'string'}
     {@const href = `/svelte-canvas${value}`}
-    <a class:active={$page.url.pathname === href} {href}>{title}</a>
+    <a class:active={$page.url.pathname.replace(/\/$/, '') === href} {href}>
+      {title}
+    </a>
   {:else if typeof value === 'object'}
     <p>{title}</p>
     <svelte:self items={value} />
