808b70fead02a7a90e77b659a9e99b4c0353838e
Daniel Nass <dnlnss@gmail.com>
2024-11-26 13:13:42 -0500

    Esbuild fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
 });
